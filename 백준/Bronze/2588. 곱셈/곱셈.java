1347be706dc0731c49547dbe9cca1bff956f92ef
Gahee Yoon <a5312559@gmail.com>
2024-03-31 23:57:10 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,18 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int multiplicand = Integer.parseInt(br.readLine());
         String multiplier = br.readLine();
 
-        System.out.println(multiplicand * (multiplier.charAt(2) - '0'));
-        System.out.println(multiplicand * (multiplier.charAt(1) - '0'));
-        System.out.println(multiplicand * (multiplier.charAt(0) - '0'));
-        System.out.println(multiplicand * Integer.parseInt(multiplier));
+        sb.append(multiplicand * (multiplier.charAt(2) - '0')).append("\n");
+        sb.append(multiplicand * (multiplier.charAt(1) - '0')).append("\n");
+        sb.append(multiplicand * (multiplier.charAt(0) - '0')).append("\n");
+        sb.append(multiplicand * Integer.parseInt(multiplier)).append("\n");
+        
+        System.out.println(sb);
         
         br.close();
     }
