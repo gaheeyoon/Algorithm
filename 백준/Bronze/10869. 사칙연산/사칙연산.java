63f673c47092306a408b669c3486177343486428
Gahee Yoon <a5312559@gmail.com>
2024-03-31 23:51:44 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,18 +1,22 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
+        sb.append(a + b).append("\n");
+        sb.append(a - b).append("\n");
+        sb.append(a * b).append("\n");
+        sb.append(a / b).append("\n");
+        sb.append(a % b).append("\n");
+        
+        System.out.println(sb);
         
         br.close();
     }
