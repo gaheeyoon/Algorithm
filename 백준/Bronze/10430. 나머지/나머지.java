46ded39025a31083715ca2329371de492da94485
Gahee Yoon <a5312559@gmail.com>
2024-03-31 23:54:39 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,21 +1,21 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        System.out.println((a + b) % c);
-        System.out.println(((a % c) + (b % c)) % c);
-        System.out.println((a * b) % c);
-        System.out.println(((a % c) * (b % c)) % c);
+        sb.append((a + b) % c).append("\n");
+        sb.append(((a % c) + (b % c)) % c).append("\n");
+        sb.append((a * b) % c).append("\n");
+        sb.append(((a % c) * (b % c)) % c).append("\n");
+        System.out.println(sb);
         
         br.close();
     }
