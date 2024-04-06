7f5129135d7a6819a834b40cd7ceb3f403ffa9e5
Gahee Yoon <a5312559@gmail.com>
2024-04-07 08:47:04 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String line;
+		
+		while( (line = br.readLine()) != null ) {
+            int a = (line.charAt(0) - 48);
+            int b = (line.charAt(2) - 48);
+            
+            sb.append(a + b).append("\n");
+		}
+		
+        System.out.println(sb);
+        br.close();
+	}
+}
